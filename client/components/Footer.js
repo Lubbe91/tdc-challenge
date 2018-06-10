d613ced208846603ea02f477d846fcfdbf8d5b5b
Patrick <Patrick_brockmann@hotmail.com>
2018-06-10 11:44:07 +0200

    added a footer & changed style for margin on body & added style for footer

---
+++ client/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default class Footer extends React.Component {
+
+  render() {
+    return (
+        <p>Lavet af Patrick Stefan Brockmann til TDC frontend challenge</p>
+    )
+  }
+}
--- client/index.js
+++ client/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
+import Footer from './components/Footer.js';
 
 import './styles/main.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Footer />, document.getElementById('footer'));
